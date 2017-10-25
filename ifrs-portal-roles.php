fb2cdd29f3aa844f8a4dc474d76e81dcf08f7937
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-10-25 10:54:55 -0200

    Commit inicial.

---
+++ ifrs-portal-roles.php
@@ -0,0 +1,16 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+/*
+Plugin Name: IFRS Portal Roles
+Plugin URI:  https://github.com/IFRS/portal-plugin-roles
+Description: Plugin para definir perfis de usuários.
+Version:     1.0.0
+Author:      Ricardo Moro
+Author URI:  https://github.com/ricardomoro
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+Text Domain: ifrs-portal-plugin-roles
+Domain Path: /lang
+*/
+
+require_once('roles.php');
---
+++ roles.php
@@ -0,0 +1,71 @@
+<?php
+// Fix Media Permissions
+add_action('init', function() {
+    global $wp_post_types;
+    $wp_post_types['attachment']->cap->edit_posts = 'manage_files';
+    $wp_post_types['attachment']->cap->delete_posts = 'manage_files';
+});
+
+// Roles
+register_activation_hook(__FILE__, function () {
+    if (!get_role( 'jornalista' )) {
+        add_role('jornalista', __('Jornalista'), array(
+            'read'                   => true,
+            'upload_files'           => true,
+            'edit_files'             => true,
+            'delete_files'           => false,
+
+            'publish_posts'          => true,
+            'edit_posts'             => true,
+            'edit_others_posts'      => true,
+            'edit_published_posts'   => true,
+            'delete_posts'           => true,
+            'delete_others_posts'    => true,
+            'delete_published_posts' => true,
+            'manage_categories'      => true,
+        ));
+    }
+
+    if (!get_role( 'conteudista' )) {
+        add_role('conteudista', __('Conteudista'), array(
+            'read'                   => true,
+            'upload_files'           => true,
+            'edit_files'             => true,
+            'delete_files'           => false,
+
+            'publish_pages'          => false,
+            'edit_pages'             => true,
+            'edit_others_pages'      => true,
+            'edit_published_pages'   => true,
+        ));
+    }
+
+    if (!get_role( 'gerente_conteudo' )) {
+        add_role('gerente_conteudo', __('Gerente de Conteúdo'), array(
+            'read'                   => true,
+            'upload_files'           => true,
+            'edit_files'             => true,
+            'delete_files'           => true,
+
+            'publish_pages'          => true,
+            'edit_pages'             => true,
+            'edit_others_pages'      => true,
+            'edit_published_pages'   => true,
+            'delete_pages'           => true,
+            'delete_others_pages'    => true,
+            'delete_published_pages' => true,
+        ));
+    }
+});
+
+register_deactivation_hook(__FILE__, function () {
+    if (get_role( 'jornalista' )) {
+        remove_role( 'jornalista' );
+    }
+    if (get_role( 'conteudista' )) {
+        remove_role( 'conteudista' );
+    }
+    if (get_role( 'gerente_conteudo' )) {
+        remove_role( 'gerente_conteudo' );
+    }
+});
